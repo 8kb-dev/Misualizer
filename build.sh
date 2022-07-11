515a0a0eda7d916c803b62bf4785de14019b5415
my8bit <mail@my8bit.name>
2022-07-11 19:48:47 +0200

    update lock file; change output dit to build; remove docs folder

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-rm -rf docs
+rm -rf build
 rm -rf .cache
 
 BROWSER_OPT=true \
 parcel build \
   src/web/plugin.js \
   src/web/index.html \
-  --no-source-maps --out-dir docs
+  --no-source-maps --out-dir build
 
 cp src/web/CNAME docs/
 cp src/web/.nojekyll docs/
